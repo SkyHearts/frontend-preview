9e3900a244fd95cc9b8a45687e1de4a5897f57c2
SkyHearts <null>
2024-09-07 11:25:51 +0000

    🔄 synced local 'next.config.mjs' with remote 'next.config.mjs'

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	async redirects() {
-		return [
-		  {
-			source: '/',
-			destination: '/geo',
-			permanent: true, // or false for temporary redirects
-		  },
-		]
-	},
+    
+	// async redirects() {
+	// 	return [
+	// 	  {
+	// 		source: '/',
+	// 		destination: '/geo',
+	// 		permanent: true, // or false for temporary redirects
+	// 	  },
+	// 	]
+	// },
 	eslint: {
 		ignoreDuringBuilds: true,
-	  },
+	},
 	typescript: {
 		ignoreBuildErrors: true,
-  },
+    },
+    
+  
 };
 
 export default nextConfig;
